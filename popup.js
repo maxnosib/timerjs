25745250fa9ffeee77438606b7a64ac77a3c13b8
maxnosib <maxnosib@gmail.com>
2020-02-06 21:19:42 +0300

    inisial commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function() {
+        console.log("button click");
+    }, false);
+}, false);
