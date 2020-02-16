2e782b7ca18c7ba595fad9c5c62943c0346f329b
maxnosib <maxnosib@gmail.com>
2020-02-16 15:01:50 +0300

    update logic timer

--- popup.js
+++ popup.js
@@ -1,114 +1,91 @@
+let timerId
+
+// слушаем клики по кнопкам
 document.addEventListener('DOMContentLoaded', function() {
     var start = document.getElementById('start');
-    start.addEventListener('click', function() {
-        localStorage.setItem('start_time', new Date().getTime())
-        dateObj = new Date();
-        StartTIME();
-    }, false);
     var clear = document.getElementById('clear');
+    var pause = document.getElementById('pause');
+
+    start.addEventListener('click', function() {
+        start.disabled = true;
+        pause.disabled = false;
+        startTimer()
+    }, false);
     clear.addEventListener('click', function() {
+        start.disabled = false;
+        pause.disabled = false;
         clearClock();
     }, false);
+    pause.addEventListener('click', function() {
+        start.disabled = false;
+        pause.disabled = true;
+        pauseClock();
+    }, false);
+
 }, false);
 
+// функция для запуска таймера при повторном открытии приложения
 function checkLS() {
     if (localStorage.getItem('start_time') != "") {
-        dateObj = new Date(Number(localStorage.getItem('start_time')));
-        StartTIME();
+        timerId = setInterval(() => timer(new Date().getTime()), 1000);
+    }
+    isPause = localStorage.getItem('is_pause') == 'true';
+    if (isPause) {
+        document.getElementById('pause').disabled = true;
+    } else {
+        document.getElementById('start').disabled = true;
     }
 }
 window.onload = checkLS;
 
-
-//объявляем переменные
-var dateObj = new Date();
-var base = 60;
-var clocktimer, dateObj, dh, dm, ds, ms;
-var readout = '';
-var h = 1,
-    m = 1,
-    tm = 1,
-    s = 0,
-    ts = 0,
-    ms = 0,
-    init = 0;
-
-
-
-//функция для очистки поля
-function clearClock() {
-    clearTimeout(clocktimer);
-    h = 1;
-    m = 1;
-    tm = 1;
-    s = 0;
-    ts = 0;
-    ms = 0;
-    init = 0;
-    readout = '00:00:00';
-    document.MyForm.stopwatch.value = readout;
+// функция подсчета времени
+let timer = function(curentTime) {
+    startTime = Number(localStorage.getItem('start_time'))
+    diffTime = (curentTime - startTime) / 1000
+    hour = diffTime / 3600
+    if (hour < 1) {
+        hour = 0
+    } else {
+        hour = Math.trunc(hour)
+    }
+    min = (diffTime - hour * 3600) / 60
+    if (min < 1) {
+        min = 0
+    } else {
+        min = Math.trunc(min)
+    }
+    sec = diffTime - hour * 3600 - min * 60
+    sec = Math.trunc(sec)
+    readout = hour + ':' + min + ':' + sec;
+    isPause = localStorage.getItem('is_pause') == 'true';
+    if (!isPause) {
+        document.getElementById('stopwatch').value = readout;
+    }
 }
 
-//функция для старта секундомера
-function StartTIME() {
-    var cdateObj = new Date();
-    var t = (cdateObj.getTime() - dateObj.getTime()) - (s * 1000);
-    if (t > 999) {
-        s++;
+function startTimer() {
+    isPause = localStorage.getItem('is_pause') == 'true';
+    difTime = 0
+    if (isPause) {
+        difTime = Number(localStorage.getItem('diff_time'));
     }
-    if (s >= (m * base)) {
-        ts = 0;
-        m++;
-    } else {
-        ts = parseInt((ms / 100) + s);
-        if (ts >= base) {
-            ts = ts - ((m - 1) * base);
-        }
-    }
-    if (m > (h * base)) {
-        tm = 1;
-        h++;
-    } else {
-        tm = parseInt((ms / 100) + m);
-        if (tm >= base) {
-            tm = tm - ((h - 1) * base);
-        }
-    }
-    ms = Math.round(t / 10);
-    if (ms > 99) {
-        ms = 0;
-    }
-    if (ms == 0) {
-        ms = '00';
-    }
-    if (ms > 0 && ms <= 9) {
-        ms = '0' + ms;
-    }
-    if (ts > 0) {
-        ds = ts;
-        if (ts < 10) {
-            ds = '0' + ts;
-        }
-    } else {
-        ds = '00';
-    }
-    dm = tm - 1;
-    if (dm > 0) {
-        if (dm < 10) {
-            dm = '0' + dm;
-        }
-    } else {
-        dm = '00';
-    }
-    dh = h - 1;
-    if (dh > 0) {
-        if (dh < 10) {
-            dh = '0' + dh;
-        }
-    } else {
-        dh = '00';
-    }
-    readout = dh + ':' + dm + ':' + ds;
+    localStorage.setItem('is_pause', false);
+    localStorage.setItem('start_time', new Date().getTime() - difTime);
+    timerId = setInterval(() => timer(new Date().getTime()), 1000);
+}
+
+// функция для очистки поля
+function clearClock() {
+    clearInterval(timerId);
+    localStorage.removeItem('start_time');
+    localStorage.setItem('is_pause', true);
+    readout = '0:0:0';
     document.getElementById('stopwatch').value = readout;
-    clocktimer = setTimeout("StartTIME()", 1);
+}
+
+// функция для паузы
+function pauseClock() {
+    startTime = Number(localStorage.getItem('start_time'))
+    localStorage.setItem('diff_time', new Date().getTime() - startTime);
+    localStorage.setItem('is_pause', true);
 }
